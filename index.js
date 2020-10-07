9ce15c23fd38f3b67b1864deb2ad7d82016b4219
nickubed <nhpquandt@gmail.com>
2020-10-07 16:03:06 -0700

    ready to roll

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ require('dotenv').config()
 const axios = require('axios')
 const express = require('express')
 const layouts = require('express-ejs-layouts')
-const app = express()
-const PORT = process.env.PORT
 const methodOverride = require('method-override')
-let db = require('./models')
+const PORT = process.env.PORT
+
+const app = express()
+const db = require('./models')
 
 app.set('view engine', 'ejs')
 app.use(layouts)
 app.use('/', express.static('static'))
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
-app.use('/beans', require('./controllers/beans'))
 
-// app.get('/', (req, res) => {
-//     res.send('Welcome home')
-// })
+app.use('/beans', require('./controllers/beans'))
 
 app.get('/', (req, res) => {
     db.Bean.find((err, beans) => {
@@ -30,4 +28,4 @@ app.get('/', (req, res) => {
     })
 })
 
-app.listen(PORT || 4000, () => {console.log(`Churnin n Burning on port ${PORT}`)})
+app.listen(PORT || 4000, () => {console.log(`Churnin n Burnin on port ${PORT}`)})
